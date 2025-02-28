a815854e48f0662819236eef9b8c41e17235a15d
yellowisk <oheitoralmeida2013@gmail.com>
2025-02-28 15:29:50 -0300

    feat(Editor): pass content from localStorage as prop

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import RichTextEditor from "@/components/RichTextEditor";
 
 export default function Editor() {
+    const content = localStorage.getItem('content') || '';
+
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center">
-            <RichTextEditor content={ "" } />
+            <RichTextEditor content={ content } />
         </div>
     )
 }
