a3008b0ffeb65ef18ba6f391c0ed9d2e4d557fcb
yellowisk <oheitoralmeida2013@gmail.com>
2025-02-28 16:46:10 -0300

    rm pass content from localStorage to RichTextEditor

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -2,11 +2,10 @@
 import RichTextEditor from "@/components/RichTextEditor";
 
 export default function Editor() {
-    const content = localStorage.getItem('content') || '';
 
-    return (
-        <div className="h-screen w-screen flex flex-col items-center justify-center">
-            <RichTextEditor content={ content } />
-        </div>
-    )
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <RichTextEditor content={""} />
+    </div>
+  );
 }
